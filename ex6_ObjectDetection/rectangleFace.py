9f1cc2727957310455a736c490b5d1ab000a3c10
Janez Vegan <janez.vegan@gmail.com>
2022-12-27 22:29:02 +0100

    eye and face rectangle

---
+++ ex6_ObjectDetection/rectangleFace.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+video=cv2.VideoCapture(1)
+
+while 1:
+    b, imgJaz=video.read()
+    
+    face_cascade = cv2.CascadeClassifier('DATA/haarcascades/haarcascade_frontalface_default.xml')
+
+
+    def detect_face(img):
+        face_img=img.copy()
+
+        face_rects=face_cascade.detectMultiScale(face_img)
+
+        for (x,y,w,h) in face_rects:
+            cv2.rectangle(face_img,(x,y),(x+w,y+h),(255,255,255),10)
+        
+        return face_img
+
+    result = detect_face(imgJaz)
+
+    cv2.imshow('jaz',result)
+
+    if cv2.waitKey(700)& 0xFF == ord('q'):
+        break
+
+
+print('Konec')
