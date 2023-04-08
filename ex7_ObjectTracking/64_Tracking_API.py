ae6cd0dbadadf360fa1aa0b6f8335707cb2332d8
Janez Vegan <janez.vegan@gmail.com>
2023-04-08 20:11:03 +0200

    komentarji v tracking programu

--- ex7_ObjectTracking/64_Tracking_API.py
+++ ex7_ObjectTracking/64_Tracking_API.py
@@ -8,31 +8,7 @@ ye=-1
 drawing = False
 rioSet=False
 
-
-tracker = cv2.TrackerCSRT_create()
-
-def ask_for_tracker():
-    print("What Tracker API would you like to use?")
-    print("0: ")
-    print("1: ")
-    print("2: ")
-    print("3: ")
-    choice = input("Please select your tracker:")
-
-    if choice == '0':
-        tracker = cv2.TrackerMIL_create()
-    if choice == '1':
-        tracker = cv2.TrackerKCF_create()
-    if choice == '2':
-        tracker = cv2.TrackerCSRT_create()
-    if choice == '3':
-        tracker = cv2.legacy_TrackerMOSSE()
-    
-
-video = cv2.VideoCapture(1)
-b, img = video.read()
-
-
+# funkcija za risanje kvadrata in določanja RIO
 def draw_rectangle(event, x, y, flags, params):
     global xs, ys, xe, ye, drawing, img
     global rioSet
@@ -52,20 +28,43 @@ def draw_rectangle(event, x, y, flags, params):
         drawing = False
         rioSet=True
         return 
-
-cv2.namedWindow(winname = "Title of Popup Window")
-cv2.setMouseCallback('Title of Popup Window', draw_rectangle)
+    
+    
+video = cv2.VideoCapture(1)
+cv2.namedWindow(winname = "Tracking Window")
+cv2.setMouseCallback('Tracking Window', draw_rectangle)
 while True:
     b, img = video.read()
     if drawing:
         cv2.rectangle(img, pt1 =(xs, ys), pt2 =(xe, ye),
                 color =(0, 0, 255),
                 thickness =3)
-    cv2.imshow('Title of Popup Window',img)
+    cv2.imshow('Tracking Window',img)
     if cv2.waitKey(10)==ord('q') or rioSet==True:
         break
 
-print("******* Konec ***** ")
+
+def ask_for_tracker():
+    print("What Tracker API would you like to use?")
+    print("0: ")
+    print("1: ")
+    print("2: ")
+    print("3: ")
+    choice = input("Please select your tracker:")
+
+    if choice == '0':
+        tracker = cv2.TrackerMIL_create()
+    if choice == '1':
+        tracker = cv2.TrackerKCF_create()
+    if choice == '2':
+        tracker = cv2.TrackerCSRT_create()
+    if choice == '3':
+        tracker = cv2.legacy_TrackerMOSSE()
+
+# Default tracker agoritem
+tracker = cv2.TrackerCSRT_create()
+ask_for_tracker()
+
 rioImg=img[ys:ye, xs:xe]
 cv2.imshow('rio',rioImg)
 cv2.waitKey()
