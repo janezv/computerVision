3ec58dbd9cc11d101e7f72de94e84a2c5effbc25
Janez Vegan <janez.vegan@gmail.com>
2024-03-03 20:17:10 +0100

    delujočo sestavljanje videa iz različnih slik

--- Tools.py
+++ Tools.py
@@ -62,23 +62,31 @@ def assembleVideo(folder_path):
     output_video = folder_path+'/output_video.avi'
 
     # Get all PNG files in the folder
-    png_files = [f for f in os.listdir(folder_path) if f.endswith('.png')]
+    png_files = [f for f in os.listdir(
+        folder_path) if f.endswith('.png') or f.endswith('.jpg')]
     png_files.sort()
+    if len(png_files) == 0:
+        print("Ni slik v mapi !!! MAPA JE PRAZNA")
+        return
+    try:
+        averageHeight, averageWidth = PictureAverageWidthHeight(folder_path)
+        averageIntHeight = int(averageHeight)
+        averageIntWidth = int(averageWidth)
+        resize_images(folder_path, averageIntHeight, averageIntWidth)
+        fps = 1  # Set the frame rate (frames per second)
+        # Define video codec and create VideoWriter object
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        video_writer = cv2.VideoWriter(
+            output_video, fourcc, fps, (averageIntHeight, averageIntWidth))
 
-    averageHeight, averageWidth = PictureAverageWidthHeight(folder_path)
-    averageIntHeight = int(averageHeight)
-    averageIntWidth = int(averageWidth)
-    resize_images(folder_path, averageIntHeight, averageIntWidth)
-    # Define video codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    video_writer = cv2.VideoWriter(
-        output_video, fourcc, 10.0, (averageIntHeight, averageIntWidth))
+        # Write each frame to the video
+        for png_file in png_files:
+            image = cv2.imread(os.path.join(folder_path, png_file))
+            video_writer.write(image)
 
-    # Write each frame to the video
-    for png_file in png_files:
-        image = cv2.imread(os.path.join(folder_path, png_file))
-        video_writer.write(image)
+        # Release VideoWriter object
+        video_writer.release()
+        print(f"Video assembled successfully: {output_video}")
 
-    # Release VideoWriter object
-    video_writer.release()
-    print(f"Video assembled successfully: {output_video}")
+    except Exception as e:
+        print(f"Error processing {e}: {e}")
